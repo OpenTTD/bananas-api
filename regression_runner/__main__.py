6d9aa19b01dae501e3f618c7aa1197b54d1920f1
erenes <erik_renes@hotmail.com>
2021-04-18 21:08:28 +0200

    Fix: Update regression runner to work with redirect login

--- regression_runner/__main__.py
+++ regression_runner/__main__.py
@@ -108,6 +108,14 @@ async def handle_user_login(step):
         f"code_challenge={code_challenge}&"
         f"code_challenge_method=S256",
     )
+    if result.status != 302:
+        raise RegressionFailure(f"Was not redirected on login; status_code={result.status}")
+    # the redirect location is a full url, we should strip the hostname to have api_call swallow it
+    redirect_url = result.headers["location"]
+    if redirect_url.startswith("http://127.0.0.1:8080"):
+        redirect_url = redirect_url[21:]
+
+    result = await api_call("GET", redirect_url)
     if result.status != 200:
         raise RegressionFailure(f"Couldn't login; status_code={result.status}")
 
