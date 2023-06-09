4b3ea465b822422a4f29a136dcfa4d1f88020fb0
Patric Stout <truebrain@openttd.org>
2023-06-09 12:21:18 +0200

    Add: allow defining what S3 endpoint URL to use
    This is needed to access S3-compatible buckets, like on Cloudflare R2.

--- bananas_api/storage/s3.py
+++ bananas_api/storage/s3.py
@@ -5,6 +5,7 @@ import os
 from openttd_helpers import click_helper
 
 _bucket_name = None
+_endpoint_url = None
 
 
 class Storage:
@@ -12,7 +13,7 @@ class Storage:
         if _bucket_name is None:
             raise Exception("--storage-s3-bucket has to be given if storage is s3")
 
-        self._s3 = boto3.client("s3")
+        self._s3 = boto3.client("s3", endpoint_url=_endpoint_url)
 
     def move_to_storage(self, filename, content_type, unique_id, md5sum):
         folder = f"{content_type.value}/{unique_id}"
@@ -28,7 +29,13 @@ class Storage:
     "--storage-s3-bucket",
     help="Name of the bucket to upload the files. (storage=s3 only)",
 )
-def click_storage_s3(storage_s3_bucket):
-    global _bucket_name
+@click.option(
+    "--storage-s3-endpoint-url",
+    help="S3 endpoint URL to reach the bucket. (storage=s3 only)",
+)
+def click_storage_s3(storage_s3_bucket, storage_s3_endpoint_url):
+    global _bucket_name, _endpoint_url
 
     _bucket_name = storage_s3_bucket
+    if storage_s3_endpoint_url:
+        _endpoint_url = storage_s3_endpoint_url
