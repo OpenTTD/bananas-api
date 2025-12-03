50476e384278f9fdfc7888f975ed5c1a71401d7e
Rubidium <rubidium@openttd.org>
2025-12-03 20:02:41 +0100

    Fix #523: metadata:url was not allowed for base sets

--- bananas_api/new_upload/readers/helpers/base_sets.py
+++ bananas_api/new_upload/readers/helpers/base_sets.py
@@ -19,6 +19,9 @@ class BaseSet:
     @ivar description: Description of Base Set
     @type description: C{str}
 
+    @ivar url: URL of Base Set
+    @type url: C{str}
+
     @ivar files: Files included in Base Set, with their md5sum
     @type files: C{dict}
     """
@@ -30,6 +33,7 @@ class BaseSet:
         self.unique_id = None
         self.version = None
         self.description = None
+        self.url = None
         self.files = None
 
     def read(self, fp):
@@ -63,9 +67,12 @@ class BaseSet:
             # Check entries we expected but are not there
             for key, key_type in keys.items():
                 if not ini_parser.has_option(section, key):
+                    if key_type == str | None:
+                        continue
                     raise ValidationException(f"Option {section}:{key} is missing.")
+
                 value = ini_parser.get(section, key)
-                if not isinstance(value, key_type):
+                if not isinstance(value, str if key_type == str | None else key_type):
                     raise ValidationException(f"Option {section}:{key} is not a {key_type}.")
 
         # List the files, their md5sum, and the complete md5sum
@@ -95,5 +102,6 @@ class BaseSet:
         self.unique_id = ini_parser.get("metadata", "shortname").encode()
         self.version = ini_parser.get("metadata", "version")
         self.description = ini_parser.get("metadata", "description")
+        self.url = ini_parser.get("metadata", "url", fallback=None)
         self.files = files
         self.md5sum = md5sum
